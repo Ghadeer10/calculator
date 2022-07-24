58bbe28fd2b3429c755d69be35ae3c9e00642c06
Ghadeer10 <86905157+Ghadeer10@users.noreply.github.com>
2022-07-24 15:38:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,59 +4,25 @@ let opreator = '';
 let operIndex = '';
 result.value = 0;
 
-//for keyboard:
-result.onkeypress = function (e) {
-    var event = e || window.event;
-    var charCode = event.which || event.keyCode;
-    var charStr = String.fromCharCode(charCode);
-    if (charStr === '0' || charStr === '1' || charStr === '2' || charStr === '3' || charStr === '4' || charStr === '5' ||
-        charStr === '6' || charStr === '7' || charStr === '8' || charStr === '9' || charStr === '.' || charStr === '+' ||
-        charStr === '-' || charStr === '*' || charStr === '/') {
-        if (result.value == 0) { result.value = ''; }
-    }
-    else if (charCode == '13') {
-        operate();
-    }
-    else {
-        alert('no alphabet allowed to enter for now, so delete it');
-    }
-}
 
 //function for entering number:
 function enterNumber(num) {
     if (result.value === "0") {
-        result.value = '';
-        result.value += num;
+        if (num == "0" || num == '00') { }
+        else {
+            result.value = '';
+            result.value += num;
+        }
     }
     else {
         result.value += num;
     }
 }
 
-//function for entering dot
-function enterDot() {
-    if (opreator === '' && !result.value.includes('.')) {
-        result.value += '.';
-    }
-    else if (opreator !== '') {
-        let i = result.value.length - 1;
-        if (result.value[i] === opreator) {
-            result.value += '0.';
-        }
-        else {
-            let secNum = result.value.slice(operIndex);
-            if (!secNum.includes('.')) {
-                result.value += '.';
-            }
-        }
-    }
-}
-
-//function for entering operator and operand
+//function for entering operator
 function addOperator(op) {
     let i = result.value.length - 1;
-    let m = result.value[i];
-    console.log("vslue{i} = " + m);
+    let lastChar = result.value[i];
     try {
         if (result.value === "") {
             alert('enter number first');
@@ -64,27 +30,22 @@ function addOperator(op) {
         else {
             if (opreator === '') {
                 opreator = op;
-                firstNum = result.value.slice(0, -1);
                 result.value += op;
-                operIndex = result.value.length - 1;
-                console.log(operIndex)
+                firstNum = +result.value.slice(0, -1);
+                operIndex = i;
             }
-            else if (opreator !== '') {
-                if (m === '/' || m === '*' || m === '-' || m === '+') {
+            else {
+                if (lastChar == '/' || lastChar == '*' || lastChar == '-' || lastChar == '+') {
                     result.value = result.value.slice(0, -1);
                     result.value += op;
                     opreator = op;
-                    console.log("operIndex = " + operIndex);
                 }
                 else {
-                    console.log(secondNum);
-                    total = eval(result.value);
-                    result.value = total;
-                    firstNum = total;
+                    secondNum = +result.value.slice(operIndex + 1);
+                    total = operate();
                     opreator = op;
                     result.value += op;
                     operIndex = result.value.indexOf(op);
-                    console.log("operIndex = " + operIndex);
                 }
             }
         }
@@ -94,12 +55,73 @@ function addOperator(op) {
     }
 }
 
+//function for decimal point
+function enterDot() {
+    if (opreator === '' && !result.value.includes('.')) {
+        result.value += '.';
+    }
+    else if (opreator !== '') {
+        let i = result.value.length - 1;
+        if (i == operIndex) {
+            result.value += '0.';
+        }
+        else {
+            let secondNumAsStr = result.value.slice(operIndex + 1);
+            if (!secondNumAsStr.includes('.')) {
+                result.value += '.';
+            }
+        }
+    }
+}
+
 //function for operateing the result
 function operate() {
     try {
-        result.value = eval(result.value);
-        opreator = '';
-        operIndex = '';
+        switch (opreator) {
+            case '+':
+                secondNum = +result.value.slice(operIndex + 1);
+                total = +firstNum + +secondNum;
+                total = Math.round((total + Number.EPSILON) * 10000000) / 10000000;
+                result.value = total;
+                firstNum = +total;
+                opreator = '';
+                secondNum = 0;
+                operIndex = '';
+                break;
+            case '-':
+                secondNum = +result.value.slice(operIndex + 1);
+                total = +firstNum - +secondNum;
+                total = Math.round((total + Number.EPSILON) * 10000000) / 10000000;
+                result.value = total;
+                firstNum = +total;
+                secondNum = 0;
+                opreator = '';
+                operIndex = '';
+                break;
+            case '*':
+                secondNum = +result.value.slice(operIndex + 1);
+                total = +firstNum * +secondNum;
+                total = Math.round((total + Number.EPSILON) * 10000000) / 10000000;
+                result.value = total;
+                firstNum = +total;
+                secondNum = 0;
+                opreator = '';
+                operIndex = '';
+                break;
+            case '/':
+                secondNum = +result.value.slice(operIndex + 1);
+                total = +firstNum / +secondNum;
+                total = Math.round((total + Number.EPSILON) * 100000) / 100000;
+                result.value = total;
+                firstNum = +total;
+                opreator = '';
+                secondNum = 0;
+                operIndex = '';
+                break;
+            default:
+                alert("Enter valid mathmatical operation");
+                break;
+        }
     }
     catch (err) {
         alert("Enter valid mathmatical operation");
@@ -110,6 +132,9 @@ function operate() {
 function clearAll() {
     try {
         result.value = 0;
+        total = 0;
+        firstNum = 0;
+        secondNum = 0;
         operIndex = '';
         opreator = '';
     }
@@ -118,10 +143,10 @@ function clearAll() {
     }
 }
 
-//function for deleting the last entered charactter
+//functio for  delete last char
 function backspace() {
     try {
-        if (result.value === '0') { alert("why are you delete nothing?!"); }
+        if (result.value == '0') { alert("why are you delete nothing?!"); }
         else {
             let i = result.value.length - 1;
             if (result.value[i] == opreator) {
@@ -131,6 +156,7 @@ function backspace() {
             }
             else {
                 result.value = result.value.slice(0, -1);
+                secondNum = +result.value.slice(operIndex + 1);
                 if (result.value == '') {
                     result.value = 0;
                 }
@@ -138,121 +164,6 @@ function backspace() {
         }
     }
     catch (err) {
-        alert(err);
+        alert('oops', err);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // let secondNumber = document.getElementById("#secondNum");
-// // let plusBtn = document.querySelector('#plus');
-
-// // plusBtn.addEventListener('click', function () {
-// //     alert(firstNumber + secondNumber);
-// // })
-
-// // let subBtn = document.querySelector('#sub');
-
-// // subBtn.addEventListener('click', function () {
-// //     alert(firstNumber - secondNumber);
-// // })
-
-
-// //creat function to handle btns
-// function getNumbers(num) {
-
-//     if (!values.prev) {
-//         values.prev = num;
-//     }
-//     else {
-//         values.new = num;
-//     }
-//     screen.value = num;
-//     console.log(num);
-// }
-
-// function getOpreators(op) {
-//     operType = op;
-//     console.log(values);
-// }
-
-// function calculate() {
-//     if (!values.new) {
-//         return;
-//     }
-//     if (values.new && values.prev && operType) {
-//         if (operType === "+") {
-//             screen.value = values.new + values.prev;
-//         }
-//         if (operType === "-") {
-//             screen.value = values.prev - values.new;
-//         }
-//         if (operType === "*") {
-//             screen.value = values.new * values.prev;
-//         }
-//         if (operType === "/") {
-//             screen.value = values.prev / values.new;
-//         }
-//     }
-// }
-// let screen = document.getElementById('screen');
-// let values = { prev: null, new: null, result: null };
-// let operType = '';
-
-//create function to handle btns opreator
-
-//display number value in the input
-
-//handle privous value
-
-//handle opreator
-
-//handle new value
-
-//create equall functionality
-
-//1 handle total number
-// 2- handle new calculation
-// 3- handle numbers bigger
